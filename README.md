b1954b09c5bd3df3f2eeba253f01911e640233d8
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-12-31 22:41:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Session user as the default sale order mail sender
+---------------------------------------------------
+This module set the session user as the default mail sender
+
+
---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*- 
+
+
+{
+    'name': 'Session user as the default sale order mail sender',
+    'author': 'Soft-integration',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'qweb': [],
+    'description': False,
+    'images': [],
+    'version': '1.0.1',
+    'category': 'Sale',
+    'demo': [],
+    'depends': ['sale'],
+    'data': [
+        'data/mail_templates_data.xml',
+    ],
+    'license': 'LGPL-3',
+}
